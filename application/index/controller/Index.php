998847a0b97e728888e5e361b09bee9bc41bc35b
zhaoshang22 <2571953385@qq.com>
2017-12-04 16:10:43 +0800

    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 use TestWeChat\Class_weixin_adv;
+use think\Controller\redirect;
 class Index
 {
 
@@ -24,14 +25,14 @@ class Index
 
       $row=$weixin->get_user_info($res['openid']); 
       $openid = $row['openid']; //获取openid
-      return $this->redirect('Index/wechat', ['openid' => $openid]);
+      return redirect('Index/wechat',['openid'=>$openid]);
       // $unionid = $row['unionid'];  //获取uniond
       
-      print_r($res);
-      print_r($row);
+      // print_r($res);
+      // print_r($row);
     }
     public function wechat($openid){
-      
+
       return view('wechat',['info'=>$openid]);
 
     } 
